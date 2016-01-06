42ce4a745df05c6e476d6b19290f3cb023ebb648
haint <haint.fit12@gmail.com>
2016-01-06 22:05:19 +0700

    Remove unnecessary directory

--- jumpmanga/src/main/java/io/demiseq/jetreader/utils/AsyncTaskCallback.java
+++
@@ -1,9 +0,0 @@
-package io.demiseq.jetreader.utils;
-
-/**
- * Created by Thanh on 7/24/2015.
- */
-public interface AsyncTaskCallback {
-    void onTaskStarted();
-    void onTaskFinished();
-}
--- jumpmanga/src/main/java/io/demiseq/jetreader/utils/OnLoadMoreListener.java
+++
@@ -1,8 +0,0 @@
-package io.demiseq.jetreader.utils;
-
-/**
- * Created by Thanh on 7/3/2015.
- */
-public interface OnLoadMoreListener {
-    void onLoadMore();
-}
