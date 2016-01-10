ca60b51f507959938c8d18232a4b88d10b4f0539
haint <haint.fit12@gmail.com>
2016-01-10 09:26:33 +0700

    Properly recreate parent activity in case it gets killed in low memory environment

--- jetreader/src/main/java/io/demiseq/jetreader/activities/BaseActivity.java
+++ jetreader/src/main/java/io/demiseq/jetreader/activities/BaseActivity.java
@@ -2,6 +2,7 @@ package io.demiseq.jetreader.activities;
 
 import android.content.Intent;
 import android.support.v4.app.NavUtils;
+import android.support.v4.app.TaskStackBuilder;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 
@@ -14,8 +15,12 @@ public class BaseActivity extends AppCompatActivity {
         switch (id) {
             case android.R.id.home:
                 Intent intent = NavUtils.getParentActivityIntent(this);
-                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
-                NavUtils.navigateUpTo(this, intent);
+                if (NavUtils.shouldUpRecreateTask(this, intent))
+                    TaskStackBuilder.create(this).addNextIntentWithParentStack(intent).startActivities();
+                else {
+                    intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+                    NavUtils.navigateUpTo(this, intent);
+                }
                 return true;
         }
 
